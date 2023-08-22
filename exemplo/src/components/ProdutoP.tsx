e2f2e67d6a3514c4d6b8c8dc903bc7c206b92fa1
Daniel Pires <dfpires@gmail.com>
2023-08-22 18:11:52 -0300

    Add files via upload

--- exemplo/src/components/ProdutoP.tsx
+++ exemplo/src/components/ProdutoP.tsx
@@ -1,11 +1,36 @@
 import { Produto } from "./Produto";
 import {useLocation} from 'react-router-dom';
+import {useState, useEffect} from 'react'
+
+interface ProdutoProps {
+  id: number,
+  name: string,
+  description: string,
+  price: number,
+  qty: number
+}
 
 export function ProdutoP() {
   // esta variável vai conter o username passado na navegação
   const location = useLocation();
   // recupera o username
   const username = location.state?.username || '';
+  // vetor de produtos
+  const [products, setProducts] = useState<ProdutoProps[]>([])
+
+  // fazer o hook useEffect para carregar os produtos 
+  // quando a página for carregada ou o username for alterado
+  useEffect( () => {
+    const buscaProdutos = async () => {
+      try {
+        const resp = await fetch(`http://localhost:3000/products`)
+      }
+      catch(error) {
+
+      }
+    }
+      buscaProdutos()
+  } , [username])
     return (
         <>
         <h1> Bem-Vindo {username} </h1>
