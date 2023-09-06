adc842a748658b2c401f3f5f68a29cdce7a387ca
Daniel Pires <dfpires@gmail.com>
2023-09-05 22:16:29 -0300

    Add files via upload

--- exemplo/src/components/ProdutoP.tsx
+++ exemplo/src/components/ProdutoP.tsx
@@ -1,7 +1,7 @@
 
 import {useLocation} from 'react-router-dom';
 import {useState, useEffect} from 'react'
-import {MdShoppingCart, MdSell, MdOutlineDeleteOutline} from 'react-icons/md'
+import {MdMode, MdShoppingCart, MdSell, MdOutlineDeleteOutline} from 'react-icons/md'
 
 interface ProdutoProps { // tipo de dado
   id: number,
@@ -32,8 +32,10 @@ export function ProdutoP() {
   // variáveis de estado para os campos do formulário
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
-  const [price, setPrice] = useState('')
-  const [qty, setQty] = useState('')
+  const [price, setPrice] = useState(0)
+  const [qty, setQty] = useState(0)
+  // diferencia se vai inserir (id = 0) ou editar (id não for 0) um produto
+  const [id, setId] = useState(0)
 
   // fazer o hook useEffect para carregar os produtos da API
   // quando a página for carregada ou o username for alterado
@@ -65,16 +67,41 @@ export function ProdutoP() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault() // evita que a página seja recarregada
     // monta o objeto produto
-    const produto = {
-      name,
-      description,
-      price,
-      qty
+    console.log(`${id} handle` )
+    let produto
+    if (id == 0) { // insere
+      produto = {
+        name,
+        description,
+        price,
+        qty
+      }
+    } 
+    else {
+      produto = { // atualiza
+        name,
+        description,
+        price,
+        qty
+      }
     }
+    let url
+    let verb
+    if (id == 0) { // insere
+      url = `http://localhost:3000/products`
+      verb = 'POST'
+    }
+    else {
+      url = `http://localhost:3000/products/${id}`
+      verb = 'PUT'
+    }
+
     try {
-      // chamar a API para cadastrar o produto
-      const produtoCadastrado = await fetch(`http://localhost:3000/products`, {
-        method: 'POST',
+      // chamar a API para cadastrar o produto]
+      console.log(url)
+      console.log(verb)
+      const produtoCadastrado = await fetch(url, {
+        method: verb,
         headers: {
           'Content-Type': 'application/json'
         },
@@ -85,7 +112,19 @@ export function ProdutoP() {
       })
        // atualiza a lista de produtos
        // monta uma nova lista com a lista anterior + produto cadastrado
-      setProducts([...products, produtoCadastrado])
+       if (id == 0) { // insere
+          setProducts([...products, produtoCadastrado])
+       }
+       else { // atualiza na lista o produto alterado
+          setProducts(products.map( (product) => {
+            if (product.id === id) {
+              return produtoCadastrado
+            }
+            else {
+              return product
+            }
+          }))
+       }
       }
     catch(error) {
       console.log(error)
@@ -111,6 +150,16 @@ export function ProdutoP() {
         setProducts(products.filter( (product) => product.id !== id ))
       }
     }
+
+    const handleEdit = (product: ProdutoProps) => {
+      setName(product.name)
+      setDescription(product.description)
+      setPrice(product.price)
+      setQty(product.qty)
+      console.log(product.id)
+      setId(product.id) // vai nos ajudar na criação/edição do produto
+    }
+
     const handleBuy = async (id: number) => {
         const quantity = Number(prompt('Quantidade de produtos a comprar'))
         const price = Number(prompt('Preço do produto a comprar'))
@@ -142,9 +191,38 @@ export function ProdutoP() {
 
       }
 
-    const handleSell = async (id: number) => {
-      
-    }
+      const handleSell = async (id: number) => {
+        const quantity = Number(prompt('Quantidade de produtos a vender'))
+        const price = Number(prompt('Preço do produto a vender'))
+        // cria objeto para inserção
+        const obj = {
+          product_id: id,
+          qty: quantity,
+          price: price,
+          type: 'sell'
+        }
+        // chamamos a API para inserir a compra no banco de dados
+        const newOrder = await fetch(`http://localhost:3000/orders`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },  
+          body: JSON.stringify(obj)
+        })
+        .then( response => { // quando o servidor respondeu
+            return response.json() // transforma string em json
+        })  
+        .catch(error => {
+            alert(error)
+        })
+        
+          // atualiza a lista de orders
+       // monta uma nova lista com a lista anterior + ordem cadastrado
+      setOrders([...orders, newOrder])
+
+      }
+
+    
     return (
       <div className="flex flex-col items-center justify-center h-screen w-screen">
         <div className="max-w-md mx-auto mb-4">
@@ -171,7 +249,7 @@ export function ProdutoP() {
                 Preço
               </label>
               <input type="number" id="price" value={price}
-                      onChange={ (e) => setPrice(e.target.value) }
+                      onChange={ (e) => setPrice(Number(e.target.value)) }
                       className="w-full border border-gray-300 rounded-md shadow-sm mb-2" />
             </div>
             <div>
@@ -179,12 +257,12 @@ export function ProdutoP() {
                 Quantidade 
               </label>
               <input type="number" id="qty" value={qty}
-                      onChange={ (e) => setQty(e.target.value) }
+                      onChange={ (e) => setQty(Number(e.target.value))}
                       className="w-full border border-gray-300 rounded-md shadow-sm mb-2" />
             </div>
             <button type="submit" 
               className="w-full bg-blue-500 text-white font-bold rounded">
-              Cadastrar produto
+              Criar/Editar Produto
             </button>
           </form>
           {/* lista de produtos dentro de uma tabela */}
@@ -197,6 +275,7 @@ export function ProdutoP() {
                 <th className="border border-gray-300 px-4 py-2">Descrição</th>
                 <th className="border border-gray-300 px-4 py-2">Preço</th>
                 <th className="border border-gray-300 px-4 py-2">Quantidade</th>
+                <th className="border border-gray-300 px-4 py-2">Editar</th>
                 <th className="border border-gray-300 px-4 py-2">Remove</th>
                 <th className="border border-gray-300 px-4 py-2">Compra</th>
                 <th className="border border-gray-300 px-4 py-2">Venda</th>
@@ -211,6 +290,11 @@ export function ProdutoP() {
                     <td className="border border-gray-300 px-4 py-2">{product.description}</td>
                     <td className="border border-gray-300 px-4 py-2">{product.price}</td>
                     <td className="border border-gray-300 px-4 py-2">{product.qty}</td>
+                    <td className="border border-gray-300 px-4 py-2">
+                      <button onClick={() => handleEdit(product)}> 
+                        <MdMode size={20}/>
+                      </button>
+                    </td>
                     <td className="border border-gray-300 px-4 py-2">
                       <button onClick={() => handleRemove(product.id)}> 
                         <MdOutlineDeleteOutline size={20}/>
