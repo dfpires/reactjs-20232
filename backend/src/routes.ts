5f1e2557327bce04e2fde083395b7bd51d1ca6f7
Daniel Pires <dfpires@gmail.com>
2023-10-24 21:21:06 -0300

    Add files via upload

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,31 @@
 import { FastifyInstance } from 'fastify'
+import {prisma} from './lib/prisma'
+import {z} from 'zod'
+import dayjs from 'dayjs'
+
 export async function AppRoutes(app: FastifyInstance) {
-    app.get('/teste', async (request) => { 
-        return 'Hello World' 
+    
+    app.post('/user', async (request) => {
+
+        const postBody  = z.object({
+            username: z.string(),
+            password: z.string(), 
+            email: z.string()       
+        })
+
+        const {username, password, email} = postBody.parse(request.body)
+
+        const created_at = dayjs().startOf('day').toDate() // sem hora, minuto e segundo
+
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                password, 
+                email,
+                created_at
+            }
+        }
+        )
+        return newUser
     })
 }
